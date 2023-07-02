ee7c352d06ebc1805b6ab7f379e42c611b1bdff5
Olga Marakueva <olga_marakueva@mail.ru>
2023-07-02 12:22:30 +0300

    html templates

--- Flatrent_website/booking/urls.py
+++ Flatrent_website/booking/urls.py
@@ -3,5 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('/calendar/', views.calendar, name="calendar")
+    path('/calendar/month/', views.calendar_month, name="calendar_month"),
+    path('/calendar/year/', views.calendar_year, name="calendar_year"),
+    path('/booking/month/add/', views.booking_month_add, name="booking_month_add"),
+    path('/booking/year/add/', views.booking_year_add, name="booking_year_add"),
+    path('/booking/list/', views.booking_list, name="booking_list"),
+    path('/statistics/', views.statistics, name="statistics"),
+    path('/settings/', views.settings, name="settings"),
+    path('/settings/add/', views.settings_add, name="settings_add"),
+    path('/profile/edit/', views.profile_edit, name="profile_edit"),
+    path('/profile/registration/', views.registration, name="registration"),
+    path('/profile/entry/', views.entry, name="entry")
 ]
--- Flatrent_website/booking/views.py
+++ Flatrent_website/booking/views.py
@@ -5,6 +5,49 @@ from calendar import HTMLCalendar
 def home(request):
     return render(request, 'booking/home.html', {})
 
-def calendar(request, year=datetime.now().year, month=datetime.now().month, month_name = datetime.now().strftime("%B")):
-    cal = HTMLCalendar().formatmonth(year, month)
-    return render(request, 'booking/calendar_month.html', {"year": year, "month": month_name, "cal": cal})
+def calendar_month(request, date=datetime.now()):
+    year = date.year
+    month_name = date.strftime("%B")
+    date = date.date().strftime("%d.%m")
+    #cal = HTMLCalendar().formatmonth(year, month)
+    return render(request, 'booking/calendar_month_page.html', {"year": year, "month": month_name, "date": date})
+
+def calendar_year(request, date=datetime.now()):
+    year = date.year
+    date = date.date().strftime("%d.%m")
+    #cal = HTMLCalendar().formatmonth(year, month)
+    return render(request, 'booking/calendar_year_page.html', {"year": year, "date": date})
+
+def booking_month_add(request, date=datetime.now()):
+    year = date.year
+    month_name = date.strftime("%B")
+    date = date.date().strftime("%d.%m")
+    return render(request, 'booking/booking_month_add.html',{"year": year, "month": month_name, "date": date})
+
+def booking_year_add(request, date=datetime.now()):
+    year = date.year
+    date = date.date().strftime("%d.%m")
+    #cal = HTMLCalendar().formatmonth(year, month)
+    return render(request, 'booking/booking_year_add.html', {"year": year, "date": date})
+
+def booking_list(request):
+    return render(request, 'booking/booking_list.html', {})
+
+def statistics(request, date=datetime.now()):
+    year = date.year
+    return render(request, 'booking/statistics.html', {"year": year})
+
+def settings(request):
+    return render(request, 'booking/settings.html', {})
+
+def settings_add(request):
+    return render(request, 'booking/settings_add.html', {})
+
+def profile_edit(request):
+    return render(request, 'booking/profile_edit.html', {})
+
+def registration(request):
+    return render(request, 'booking/registration.html', {})
+
+def entry(request):
+    return render(request, 'booking/entry.html', {})
